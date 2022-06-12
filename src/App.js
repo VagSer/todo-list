28993f664b1f08204cc59056bd90cb6a04de9e7b
Sergey Vagaytsev <thevagser@gmail.com>
2022-06-12 22:29:57 +0700

    Добавил сортировку

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import ToDoList from './components/ToDoList';
 import MyHeader from './components/MyHeader';
 
@@ -11,6 +11,17 @@ function App() {
     {id: 1, text: 'Сделать проект', isDone: false},
     {id: 2, text: 'Сходить в душ', isDone: true}
   ])
+
+  const [whatShow, setWhatShow] = useState('all')
+  const [appendedTodos, setAppendedTodos] = useState(allTodos)
+
+  const showTodos = (e) => {
+    setWhatShow(e.target.value)
+    if (e.target.value === 'all') setAppendedTodos(allTodos)
+    else if (e.target.value === 'done') {
+      setAppendedTodos(allTodos.filter(todo => todo.isDone === true))
+    } else setAppendedTodos(allTodos.filter(todo => todo.isDone === false))
+  }
 
   const addTodo = (e) => {
     if (newTodo.text) {
@@ -28,7 +39,18 @@ function App() {
   return (
     <div className='App'>
       <MyHeader newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo}/>
-      <ToDoList title={allTodos.length? 'Список дел' : 'Дел нет'} todos={allTodos} removeTodo={removeTodo}/>
+        <div style={{display:'flex'}}>
+        <h4>Показать</h4>
+        <select 
+          value={whatShow}
+          onChange={(e) => showTodos(e)}
+        >
+            <option value="all">Все</option>
+            <option value="done">Сделанные</option>
+            <option value="undone">Несделанные</option>
+        </select>
+      </div>
+      <ToDoList title={allTodos.length? 'Список дел' : 'Дел нет'} todos={appendedTodos} removeTodo={removeTodo}/>
     </div>
   );
 }
